aaf67fdfa9528e1fe42146cf9d776578bd23890a
DESKTOP-PF4NOMV\h <skhan20@naver.com>
2022-09-05 11:59:13 +0900

    HSK_220905 :: 빈 수정 TEST

--- src/main/java/com/agilog/beans/BabyBean.java
+++ src/main/java/com/agilog/beans/BabyBean.java
@@ -4,5 +4,11 @@ import lombok.Data;
 
 @Data
 public class BabyBean {
-
+	private String suCode;
+	private String bbCode;
+	private String bbBirthday;
+	private String bbName;
+	private String bbHeight;
+	private String bbWeight;
+	private String bbPhoto;
 }
