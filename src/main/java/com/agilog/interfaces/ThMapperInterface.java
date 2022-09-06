6dbcdf24dc27610e4c9e9120a28025ba24600902
user <user@DESKTOP-6H041F2>
2022-09-06 09:26:29 +0900

    th 0906 :: mapper 추가

---
+++ src/main/java/com/agilog/interfaces/ThMapperInterface.java
@@ -0,0 +1,25 @@
+package com.agilog.interfaces;
+
+import java.util.List;
+
+import com.agilog.beans.AuthBean;
+import com.agilog.beans.BabyBean;
+import com.agilog.beans.BebeCalendarBean;
+import com.agilog.beans.BebeMapCommentBean;
+import com.agilog.beans.BoardBean;
+import com.agilog.beans.CompanyBean;
+import com.agilog.beans.DailyDiaryBean;
+import com.agilog.beans.DailyDiaryCommentBean;
+import com.agilog.beans.HealthDiaryBean;
+import com.agilog.beans.MyPageBean;
+import com.agilog.beans.PostBean;
+import com.agilog.beans.PostCommentBean;
+import com.agilog.beans.ReservationBean;
+import com.agilog.beans.ScheduleBean;
+
+public interface ThMapperInterface {
+
+	public CompanyBean getCompanyAccessAllInfo(CompanyBean cb);
+	public int isManagerCode(CompanyBean cb);
+
+}
