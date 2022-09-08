414d062987b8c49f71479e1bc9a72f8f86805039
DESKTOP-PF4NOMV\h <skhan20@naver.com>
2022-09-08 11:53:47 +0900

    HSK_220908 :: 사진, postBean

--- src/main/java/com/agilog/beans/PostBean.java
+++ src/main/java/com/agilog/beans/PostBean.java
@@ -4,5 +4,23 @@ import lombok.Data;
 
 @Data
 public class PostBean {
-
+	private String fbCode;
+	private String fbDate;
+	private String fbSuCode;
+	private String fbTitle;
+	private String fbContent;
+	private int fbView;
+	private int fbLike;
+	
+	private String ibCode;
+	private String ibDate;
+	private String ibTitle;
+	private String ibContent;
+	private int ibView;
+	private int ibLike;
+	
+	private int likes; //좋아요 개수
+	private boolean isLike; //좋아요 여부
+	
+	private String suCode;
 }
